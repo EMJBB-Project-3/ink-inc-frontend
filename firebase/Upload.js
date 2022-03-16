c5730d472e1c816eb2382097016e8a56feafbacd
jack-ie <jackie919@outlook.com>
2022-03-16 13:24:07 -0500

    made component for upload page

--- firebase/Upload.js
+++ firebase/Upload.js
@@ -1,10 +1,13 @@
 import React from "react";
+import ImageGrid from "../src/components/imageGrid";
 import UploadForm from "../src/components/Upload";
+import ImageGrid from "../src/components/imageGrid";
 
 function App() {
     return (
         <div className= 'App'>
             <UploadForm/>
+            <ImageGrid />
         </div>
     )
 }
