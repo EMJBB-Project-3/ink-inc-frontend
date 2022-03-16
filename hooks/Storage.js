207ed135b513bf3dd334e73d2cdea8fe9e8114d9
jack-ie <jackie919@outlook.com>
2022-03-15 21:02:39 -0500

    fireee

--- hooks/Storage.js
+++ hooks/Storage.js
@@ -4,10 +4,17 @@ import { projectStorage } from "../firebase/config";
 // used in any components that needs them
 // this function will handle file uploads and returning useful values about the upload
 const useStorage = (file) => {
-    // 
+    // progress of the upload
     const [progress, setProgress] = useState(0);
-    // 
+    // errors from the upload
     const [error, setError] = useState(null);
-    // 
+    // image url from storage
     const [url, setUrl] = useState(null);
+    
+    // takes a function as a parameter, 2nd arg is dependencies
+    // function will fire everytime file dependency changes
+    useEffect(() => {
+        // logic to upload the file
+
+    }, [file])
 }
