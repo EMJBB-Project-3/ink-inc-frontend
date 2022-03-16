28b9b0cae590b4e481856ba01f6546f7d61a787c
jack-ie <jackie919@outlook.com>
2022-03-16 17:51:15 -0500

    firebase done

---
+++ src/components/Post.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ImageGrid from "../src/components/imageGrid";
+import UploadForm from "../src/components/Upload";
+import ImageGrid from "../src/components/imageGrid";
+
+function App() {
+    return (
+        <div className= 'App'>
+            <UploadForm/>
+            <ImageGrid/>            
+        </div>
+    )
+}
+
+export default App;
