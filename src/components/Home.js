d6f32714f7d1e8620ed177beddf2f2e3cdbf637a
Evan Bulliner <evan_bulliner@yahoo.com>
2022-03-15 20:00:28 -0500

    made a post page and changed a few names around

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,17 +9,19 @@ export default function Home() {
     <Layout>
     <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
       <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['3']}>
+        <Menu.Item key="1">Home</Menu.Item>
+        <Menu.Item key="2">Post</Menu.Item>
+        <Menu.Item key="3">Profile</Menu.Item>
+        <Menu.Item key="4">About</Menu.Item>
       </Menu>
     </Header>
     <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
       <Breadcrumb style={{ margin: '16px 0' }}>
         <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
+        <Breadcrumb.Item>Post</Breadcrumb.Item>
+        <Breadcrumb.Item>Profile</Breadcrumb.Item>
+        <Breadcrumb.Item>About</Breadcrumb.Item>
       </Breadcrumb>
       <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
         <PostCard 
