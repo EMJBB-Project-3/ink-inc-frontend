b01e496ed177d5420ab0a0d095ae26bf8925f0eb
Brandon Williams <brandonawilliams1@gmail.com>
2022-03-15 20:27:30 -0500

    fixed textarea in postcard

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,13 +1,14 @@
 import LikeButton from './LikeButton'
 import { Card } from 'antd';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 // import './index.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
+import { Input } from 'antd';
 // const { Meta } = Card;
 
 const { Header, Content, Footer } = Layout;
+const { TextArea } = Input;
 
 export default function PostCard({ username, text }) {
 
@@ -39,31 +40,7 @@ export default function PostCard({ username, text }) {
                 {/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
             </Card>
 
-            <Layout className="layout">
-                <Header>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                        {new Array(4).fill(null).map((_, index) => {
-                            const key = index + 1;
-                            return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
-                        })}
-                    </Menu>
-                </Header>
-                <Content style={{ padding: '0 50px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>Post</Breadcrumb.Item>
-                        <Breadcrumb.Item>Profile</Breadcrumb.Item>
-                        <Breadcrumb.Item>About</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div className="site-layout-content">Make a comment!</div>
-                    <button>Upload an Image!</button>
-                    <button>Submit Post!</button>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>
-                    Ant Design ©2018 Created by EMJBB
-                </Footer>
-            </Layout>
+            <TextArea rows={4} placeholder="maxLength is 120" maxLength={120} />
         </>
     )
 }
