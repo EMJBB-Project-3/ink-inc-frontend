a16033e5405714b9493ef7061f1d8bc47c79d4fb
jack-ie <jackie919@outlook.com>
2022-03-19 13:59:25 -0500

    images

---
+++ src/components/firebase/config.js
@@ -0,0 +1,22 @@
+// import * as firebase from 'firebase/app';
+// import 'firebase/storage';
+// import 'firebase/firestore';
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyDDZ-iU3vq3uj4K6IeDka-YP1W2D7XAV4A",
+//   authDomain: "emjbb-dd05a.firebaseapp.com",
+//   projectId: "emjbb-dd05a",
+//   storageBucket: "emjbb-dd05a.appspot.com",
+//   messagingSenderId: "666846993380",
+//   appId: "1:666846993380:web:a6b081058b6c8e141485f1",
+//   measurementId: "G-2KLZ820JFZ"
+// };
+
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+// const projectStorage = firebase.storage();
+// const projectFirestore = firebase.firestore();
+// const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+// export { projectStorage, projectFirestore, timestamp };
