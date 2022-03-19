4c6a5604ddb538a1bc7d61441042d4f726e1e08e
Evan Bulliner <evan_bulliner@yahoo.com>
2022-03-18 22:49:28 -0500

    made a MyProfile.js page. Still making changes to the template.

---
+++ src/components/MyProfile.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Layout, Menu, Breadcrumb, Card,} from 'antd';
+
+
+export default function MyProfile() {
+
+    const { Header, Content, Footer } = Layout;
+    const { Meta } = Card;
+
+
+  return (
+  <Layout>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">Home</Menu.Item>
+        <Menu.Item key="2">Post</Menu.Item>
+        <Menu.Item key="3">My Profile</Menu.Item>
+        <Menu.Item key="4">Login</Menu.Item>
+      </Menu>
+    </Header>
+    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+      <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Post</Breadcrumb.Item>
+        <Breadcrumb.Item>My Profile</Breadcrumb.Item>
+        <Breadcrumb.Item>Login</Breadcrumb.Item>
+      </Breadcrumb>
+    <h1>Tatlover 69 Favorite Pic</h1>
+      <Card
+    hoverable
+    style={{ width: 120 }}
+    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+  >
+    <Meta title="Europe Street beat" description="www.instagram.com" />
+  </Card>
+  <h1>Tatlover 69 Favorite Pic</h1>
+  <Card
+    hoverable
+    style={{ width: 120 }}
+    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+  >
+    <Meta title="Europe Street beat" description="www.instagram.com" />
+  </Card>
+  <h1>Tatlover 69 Favorite Pic</h1>
+  <Card
+    hoverable
+    style={{ width: 120 }}
+    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+  >
+    <Meta title="Europe Street beat" description="www.instagram.com" />
+  </Card>
+      
+      {/* <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+        Content
+      </div> */}
+      
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2022 Created by EMJBB</Footer>
+  </Layout>
+    )
+}
