3546c9577b908464beaf5c2bd297916a00203cba
monstermiles <miles.emmitt.moss@gmail.com>
2022-03-21 16:15:42 -0500

    got rid of an empty div on the profile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -47,7 +47,7 @@ console.log(favoritePosts.data)
     <div>
       <Layout>
         <Header style={{ position: 'fixed', zIndex: 1, width: '100%', backgroundColor:'black' }}>
-          <div className="logo" />
+          {/* <div className="logo" /> */}
           <Menu 
           theme="dark"
            mode="horizontal" 
@@ -71,44 +71,7 @@ console.log(favoritePosts.data)
           }
 
 
-          {/* <div className="site-card-wrapper">
-    <Row gutter={16}>
-      <Col span={8}>
-      <h2>Tatlover69 favorite pic!</h2>
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-      >
-      <Meta title="Europe Street beat" description="www.instagram.com" />
-    </Card>
-      </Col>
-      <Col span={8}>
-      <h2>Tatlover69 favorite pic!</h2>
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-      >
-      <Meta title="Europe Street beat" description="www.instagram.com" />
-    </Card>
-      </Col>
-      <Col span={8}>
-      <h2>Tatlover69 favorite pic!</h2>
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-      >
-      <Meta title="Europe Street beat" description="www.instagram.com" />
-    </Card>
-      </Col>
-    </Row>
-  </div> */}
 
-          {/* <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
-        Content
-      </div> */}
 
         </Content>
         <Footer style={{ textAlign: 'center' }}>Ant Design ©2022 Created by EMJBB</Footer>
